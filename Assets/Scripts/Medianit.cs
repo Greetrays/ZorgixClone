83479466e4eb3d5beab3860b8e861597bedf6d36
Ренат <syltan.100renrgen@gmail.com>
2022-10-04 13:46:07 +0300

    add medianit

---
+++ Assets/Scripts/Medianit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Medianit : UpgradeObject
+{
+    [Header("Количество монет:")]
+    [SerializeField] private int _count;
+
+    public int Count => _count;
+}
---
+++ Assets/Scripts/UI/MoneyBar.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MoneyBar : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _money;
+    [SerializeField] private PlayerMoney _player;
+
+    private void OnEnable()
+    {
+        _player.ChangeValue += OnChange;
+    }
+
+    private void OnDisable()
+    {
+        _player.ChangeValue -= OnChange;
+    }
+
+    private void Start()
+    {
+        _money.text = _player.CurrentValue.ToString();
+    }
+
+    private void OnChange()
+    {
+        _money.text = System.Convert.ToInt32(_player.CurrentValue).ToString();
+    }
+}
